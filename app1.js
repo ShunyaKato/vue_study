dea1b592ad70564158b9a38659af06b6a0ebbe5a
ShunyaKato <shun.k0829@gmail.com>
2020-03-04 19:52:51 +0900

    make todo list "save, load localstorage"

--- app1.js
+++ app1.js
@@ -1,14 +1,7 @@
 const vm = new Vue({
   el: '#app',
   data: {
-    items: [
-      { title: '領収書を準備する', isChecked: true },
-      { title: 'Vue.jsハンズオンの資料を作る', isChecked: true },
-      { title: '参加者の人数を確認する', isChecked: false },
-      { title: 'ピザを注文する', isChecked: false },
-      { title: '参加費のお釣りを準備する', isChecked: false },
-      { title: '会場設営をする', isChecked: false },
-    ],
+    items: [],
     newItemTitle: '',
   },
   methods: {
@@ -18,11 +11,25 @@ const vm = new Vue({
         isChecked: false,
       });
       this.newItemTitle = '';
+      this.saveTodo();
     },
     deleteTodo() {
       this.items = this.items.filter(function (item) {
         return item.isChecked === false;
       });
+      this.saveTodo();
     },
-  }
+    saveTodo() {
+      localStorage.setItem('items', JSON.stringify(this.items));
+    },
+    loadTodo() {
+      this.items = JSON.parse(localStorage.getItem('items'));
+      if (!this.items) {
+        this.items = [];
+      }
+    },
+  },
+  mounted() {
+    this.loadTodo();
+  },
 })
